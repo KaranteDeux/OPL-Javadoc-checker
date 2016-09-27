b0efbbdfab42ffac8e3e88c36424c3beb063a587
Antonin Durey <dureyantonin@gmail.com>
2016-09-27 14:53:19 +0200

    Initial commit

---
+++ src/fr/univlille1/m2iagl/bacquetdurey/parser/MyClass.java
@@ -0,0 +1,27 @@
+package fr.univlille1.m2iagl.bacquetdurey.parser;
+
+/**
+ * Stupid doc for this class
+ * @author antonin
+ *
+ */
+public class MyClass {
+	
+	private int a;
+	
+	/**
+	 * Stupid javadoc for this constructor
+	 */
+	public MyClass(int a){
+		this.a = a;
+	}
+
+	/**
+	 * Stupid javadoc for this method
+	 * @return a
+	 */
+	public int getA(){
+		return a;
+	}
+
+}
